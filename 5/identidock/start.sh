0c9dcf9c4c7a1a9567d4b7a2a316cc85813c9bfe
hideaki matsunami <carbon0409@gmail.com>
2019-05-04 02:09:40 +0900

    part5 5.1 Hello World: 開発用サーバーはFlaskサーバーを使うように

--- 5/identidock/start.sh
+++ 5/identidock/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -P --name port-test -v "$(pwd)"/app:/app identidock
+# docker run --rm -P --name port-test -v "$(pwd)"/app:/app identidock
+docker run --rm -p 5000:5000 -e "ENV=DEV" --name identidock -v "$(pwd)"/app:/app identidock
