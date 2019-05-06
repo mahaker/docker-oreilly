fdad4e3f1958aa58e1730d6d8213999d75a110e6
hideaki matsunami <carbon0409@gmail.com>
2019-05-06 17:20:45 +0900

    part5 5.2: use docker-compose
    以下のコマンドで、環境ごとのコンテナを起動する。
    - 開発用サーバー（Flask）
    ```sh
    $docker-compose -f docker-compose-dev.yml up
    ```
    
    - 本番用サーバー（uWSGI）
    ```sh
    $docker-compose -f docker-compose-prod.yml up
    ```
    upのあとに-fオプションをつけると、yamlファイルを見つけられなかった
    
    upに-dオプションをつけると、デタッチモードで起動する
    docker-compose build後にupしても、ボリュームは保持されたままである。→DBやキャッシュは保持され続ける
    
    YAMLファイルのみを変更した場合は、新しいイメージは必要ないので、upコマンドを呼べばよい。

--- 5/identidock/app/identidock.py
+++ 5/identidock/app/identidock.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello mahaker!!\n'
+    return 'Hello compose!!\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
